9929acad0a353057d09817dfddb4cdbe50bbde3f
Your Name <you@example.com>
2020-09-21 20:22:14 +0300

    add locator and login_page

---
+++ pages/login_page.py
@@ -0,0 +1,22 @@
+from .base_page import BasePage
+from selenium import webdriver
+from .locators import MainPageLocators
+
+class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        # реализуйте проверку на корректный url адрес
+        assert "login" in browser.current_url, "Link is not login"
+
+    def should_be_login_form(self):
+        # реализуйте проверку, что есть форма логина
+        assert self.is_element_present(*MainPageLocators.LOGIN_FORM), "Form login is not presented"
+
+
+    def should_be_register_form(self):
+        # реализуйте проверку, что есть форма регистрации на странице
+        assert self.is_element_present(*MainPageLocators.REGIST_FORM), "Form register is not presented"

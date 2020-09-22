efe99d94841cbee2436b06f431054865e3c330ed
Your Name <you@example.com>
2020-09-22 19:24:19 +0300

    add new test metods product

--- pages/product_page.py
+++ pages/product_page.py
@@ -1,34 +1,21 @@
-from . import base_page
 from .base_page import BasePage
 from .locators import ProductPageLocators
-import time
-import math
-from selenium.common.exceptions import NoAlertPresentException
+
 
 class ProductPage(BasePage):
     def should_add_product_page(self):
         self.should_add_product_to_basket()
-        self.solve_quiz_and_get_code()
         self.should_check_name_product_to_basket()
         self.should_check_price_product_to_basket()
+
+
+    def should_not_be_success_message(self):
+        assert self.is_not_element_present(*ProductPageLocators.SUCCESS_MESSAGE), \
+            "Success message is presented, but should not be"
 
     def should_add_product_to_basket(self):
         button_add_product = self.browser.find_element(*ProductPageLocators.BUTTON_ADD)
         button_add_product.click()
-
-    def solve_quiz_and_get_code(self):
-        alert = self.browser.switch_to.alert
-        x = alert.text.split(" ")[2]
-        answer = str(math.log(abs((12 * math.sin(float(x))))))
-        alert.send_keys(answer)
-        alert.accept()
-        try:
-            alert = self.browser.switch_to.alert
-            alert_text = alert.text
-            print(f"Your code: {alert_text}")
-            alert.accept()
-        except NoAlertPresentException:
-            print("No second alert presented")
 
 
     def should_check_name_product_to_basket(self):
@@ -44,4 +31,8 @@ class ProductPage(BasePage):
         price_text_add_basked_elt = self.browser.find_element(*ProductPageLocators.PRICE_ADD_BASKED)
         price_add_basked_text = price_text_add_basked_elt.text
         assert price_text == price_add_basked_text, "The wrong price was added to the cart"
+
+    def should_is_disappeared_success_message(self):
+        assert self.is_disappeared(*ProductPageLocators.SUCCESS_MESSAGE), \
+            "The success message didn't disappear"
 

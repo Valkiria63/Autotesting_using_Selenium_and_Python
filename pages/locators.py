9fbd63a26276a3aa61d778a40d0df769aac6fd5b
Your Name <you@example.com>
2020-09-22 19:55:36 +0300

    add metod login

--- pages/locators.py
+++ pages/locators.py
@@ -3,9 +3,11 @@ from selenium.webdriver.common.by import By
 
 class MainPageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+
 class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGIST_FORM = (By.CSS_SELECTOR, "#register_form")
+
 class ProductPageLocators():
     BUTTON_ADD = (By.CSS_SELECTOR, ".btn-add-to-basket")
     NAME_PRODUCT_ADD_BASKED = (By.CSS_SELECTOR, ".alert-success strong")
@@ -13,3 +15,7 @@ class ProductPageLocators():
     PRICE = (By.CSS_SELECTOR, ".product_main .price_color")
     PRICE_ADD_BASKED = (By.CSS_SELECTOR, ".alert-info p strong")
     SUCCESS_MESSAGE = (By.CSS_SELECTOR, ".alert-success strong")
+
+class BasePageLocators():
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")

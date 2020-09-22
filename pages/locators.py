802b5bbd2621a08e7c616ecb5b7de73325b2e66e
Your Name <you@example.com>
2020-09-22 16:20:12 +0300

    add locators product page

--- pages/locators.py
+++ pages/locators.py
@@ -6,3 +6,9 @@ class MainPageLocators():
 class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGIST_FORM = (By.CSS_SELECTOR, "#register_form")
+class ProductPageLocators():
+    BUTTON_ADD = (By.CSS_SELECTOR, ".btn-add-to-basket")
+    NAME_PRODUCT_ADD_BASKED = (By.CSS_SELECTOR, ".alert-success strong")
+    NAME_PRODUCT = (By.CSS_SELECTOR, ".product_main h1")
+    PRICE = (By.CSS_SELECTOR, ".product_main .price_color")
+    PRICE_ADD_BASKED = (By.CSS_SELECTOR, ".alert-info p strong")

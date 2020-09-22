d45fa5debd3dfe6d9b03bf064456c83dfdfdd627
Your Name <you@example.com>
2020-09-22 19:25:32 +0300

    update

--- test_product_page.py
+++ test_product_page.py
@@ -5,8 +5,8 @@ import pytest
 
 
 @pytest.mark.parametrize('promo_offer', [pytest.param("7", marks=pytest.mark.xfail),
-                                         "0","1", "3", "4", "5", "6", "8", "9",])
-def test_guest_can_add_product_to_basket(browser, promo_offer):
+                                         "0","1", "3", "4", "5", "6", "8", "9"])
+def test_guest_can_add_product_to_basket(browser):
     link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{promo_offer}"
     page = MainPage(browser, link)
     page.open()
